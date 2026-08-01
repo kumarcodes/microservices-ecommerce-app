e527179ace49e27afe50160def54eafde2cf8d55
Rohit Sharma <merohitks@gmail.com>
2026-08-02 00:09:44 +0530

    Add InventoryClient and integrate inventory check in OrderService

--- order-service/src/main/java/com/rohit/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/rohit/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.rohit.microservices.order.service;
 
+import com.rohit.microservices.order.client.InventoryClient;
 import com.rohit.microservices.order.dto.OrderRequest;
 import com.rohit.microservices.order.model.Order;
 import com.rohit.microservices.order.repository.OrderRepository;
@@ -13,15 +14,23 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        //Map orderRequest to Order
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
-        //Save order to order Repository
-        orderRepository.save(order);
+
+        var isProductInStock = inventoryClient.inStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            //Map orderRequest to Order
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
+            //Save order to order Repository
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SKU " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
