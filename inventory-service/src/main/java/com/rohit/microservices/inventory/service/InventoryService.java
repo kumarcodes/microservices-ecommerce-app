4296fd70fb4e88395a23b4114277d9d599c1d0a8
Rohit Sharma <merohitks@gmail.com>
2026-08-01 20:08:20 +0530

    Add inventory management components including model, controller, service, repository, and SQL initialization

---
+++ inventory-service/src/main/java/com/rohit/microservices/inventory/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.rohit.microservices.inventory.service;
+
+import com.rohit.microservices.inventory.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public boolean isInStock(String skuCode, Integer quantity) {
+        //Find  an inventory for a given skuCode where quantity is >= 0
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+
+    }
+}
---
+++ inventory-service/src/main/resources/db/migration/V2__add-inventory.sql
@@ -0,0 +1,6 @@
+INSERT INTO `t_inventory` (`sku_code`, `quantity`)
+VALUES
+    ('iphone_17', 100),
+    ('samsung_s24', 50),
+    ('pixel_9', 75),
+    ('oneplus_12', 30);
