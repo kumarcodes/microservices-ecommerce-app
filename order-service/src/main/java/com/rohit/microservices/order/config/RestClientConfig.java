896ee5279120ebf7e775e30dbb135aa08675808d
Rohit Sharma <merohitks@gmail.com>
2026-08-05 11:47:31 +0530

    Add Eureka Discovery Server configuration with application properties and initial setup files

--- order-service/src/main/java/com/rohit/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/rohit/microservices/order/config/RestClientConfig.java
@@ -2,14 +2,14 @@ package com.rohit.microservices.order.config;
 
 import com.rohit.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.HttpClientSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
-import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
 
 import java.time.Duration;
 
@@ -18,23 +18,29 @@ public class RestClientConfig {
 
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
+
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
-                .requestFactory(getClientRequestFactory())
+                .requestFactory(clientHttpRequestFactory())
                 .build();
-        var restClientAdapter = RestClientAdapter.create(restClient);
-        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter)
-                .build();
-        return httpServiceProxyFactory.createClient(InventoryClient.class);
+
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+
+        HttpServiceProxyFactory factory =
+                HttpServiceProxyFactory.builderFor(adapter).build();
+
+        return factory.createClient(InventoryClient.class);
     }
 
-    private ClientHttpRequestFactory getClientRequestFactory() {
-        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.defaults()
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+
+        HttpClientSettings settings = HttpClientSettings.defaults()
                 .withConnectTimeout(Duration.ofSeconds(3))
                 .withReadTimeout(Duration.ofSeconds(3));
 
-        return ClientHttpRequestFactoryBuilder.detect().build(settings);
+        return ClientHttpRequestFactoryBuilder.detect()
+                .build(settings);
     }
 }
