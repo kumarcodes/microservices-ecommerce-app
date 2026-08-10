b0f28ba1d88b2ee3223f5eee95afeb74c4ca2aa7
Rohit Sharma <merohitks@gmail.com>
2026-08-11 01:15:00 +0530

    Add Kafka event publishing for order placement and update order request structure

--- order-service/src/main/java/com/rohit/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/rohit/microservices/order/service/OrderService.java
@@ -2,19 +2,24 @@ package com.rohit.microservices.order.service;
 
 import com.rohit.microservices.order.client.InventoryClient;
 import com.rohit.microservices.order.dto.OrderRequest;
+import com.rohit.microservices.order.event.OrderPlacedEvent;
 import com.rohit.microservices.order.model.Order;
 import com.rohit.microservices.order.repository.OrderRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
 
@@ -30,6 +35,11 @@ public class OrderService {
             order.setQuantity(orderRequest.quantity());
             //Save order to order Repository
             orderRepository.save(order);
+            //Send the message to Kafka Topic with Order Details(Order Number, Email)
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("Order placed event: {}", orderPlacedEvent);
+
         } else {
             throw new RuntimeException("Product with SKU " + orderRequest.skuCode() + " is not in stock");
         }
