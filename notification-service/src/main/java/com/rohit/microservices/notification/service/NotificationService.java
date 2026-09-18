c20091e6ccdc10f9af5ba83a33a6f4a30ad677aa
Rohit Sharma <merohitks@gmail.com>
2026-09-18 22:55:40 +0530

    Implement NotificationService with Kafka listener and email notification for order placement

--- notification-service/src/main/java/com/rohit/microservices/notification/service/NotificationService.java
+++ notification-service/src/main/java/com/rohit/microservices/notification/service/NotificationService.java
@@ -1,9 +1,13 @@
 package com.rohit.microservices.notification.service;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import com.rohit.microservices.notification.order.event.OrderPlacedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,4 +15,32 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class NotificationService {
 
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Got message from order-placed topic {}", orderPlacedEvent);
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("ecombyrohit@email.com");
+            messageHelper.setTo(orderPlacedEvent.getEmail());
+            messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully", orderPlacedEvent.getOrderNumber()));
+            messageHelper.setText(String.format("""
+                            Hi,
+                            
+                            Your order with order number %s is now placed successfully.
+                            
+                            Best Regards
+                            E-com by Rohit
+                            """,
+                    orderPlacedEvent.getOrderNumber()));
+        };
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order Notification email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new RuntimeException("Exception occurred when sending mail to springshop@email.com", e);
+        }
+    }
 }
